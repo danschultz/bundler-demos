06c2d62d84a83235069cd08d25a9370faad77c29
Dan Schultz <schultz.t.dan@gmail.com>
2016-04-22 17:05:46 -0700

    Compile all JS files in /src/utils

--- jspm-demo/scripts/build.sh
+++ jspm-demo/scripts/build.sh
@@ -3,5 +3,5 @@
 set -e
 
 jspm bundle react + react-dom + immutable dist/vendor.js --config jspm.bundle.js --inject
-jspm bundle src/utils/math.js + src/utils/geom.js - dist/vendor.js dist/utils.js --config jspm.bundle.js --inject
+jspm bundle src/utils/**/*.js - dist/vendor.js dist/utils.js --config jspm.bundle.js --inject
 jspm bundle src/main.js - dist/vendor.js - dist/utils.js dist/main.js --config jspm.bundle.js --inject
