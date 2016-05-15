e455472a9259d52f120989ea6df96e5dda7639f8
Dan Schultz <schultz.t.dan@gmail.com>
2016-05-15 03:01:29 -0700

    Move config into its own file

--- systemjs-demo/script/build.sh
+++ systemjs-demo/script/build.sh
@@ -3,3 +3,13 @@
 ./node_modules/babel-cli/bin/babel.js src/main.js src/views/app.js \
   --module-ids --source-root src --module-root src \
   -o dist/main.js
+
+./node_modules/babel-cli/bin/babel.js src/utils/geom.js src/utils/math.js \
+  --module-ids --source-root src --module-root src \
+  -o dist/utils.js
+
+# == This doesn't work. Each module needs to be wrapped in something
+# == SystemJS understands. Look into SystemJS builder...
+# cat node_modules/react/dist/react.js > dist/vendor.js
+# cat node_modules/react-dom/dist/react-dom.js >> dist/vendor.js
+# cat node_modules/immutable/dist/immutable.js >> dist/vendor.js
---
+++ systemjs-demo/src/systemjs.config.js
@@ -0,0 +1,23 @@
+System.config({
+  transpiler: "babel",
+  baseURL: "/",
+  defaultJSExtensions: true,
+  meta: {
+
+  },
+  packages: {
+    // "systemjs-demo": {}
+  },
+  paths: {
+    // "systemjs-demo/": "src/"
+  },
+  bundles: {
+    "dist/main.js": ["src/main.js", "src/views/app.js"],
+    "dist/utils.js": ["src/utils/geom.js", "src/utils/math.js"],
+  },
+  map: {
+    "immutable": "node_modules/immutable/dist/immutable.js",
+    "react": "node_modules/react/dist/react.js",
+    "react-dom": "node_modules/react-dom/dist/react-dom.js",
+  }
+});
